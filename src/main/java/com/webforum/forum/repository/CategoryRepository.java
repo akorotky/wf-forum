b070d2c9e37c0e3369941bea8934caee0fde6fe6
Alex Korotky <alexander.korotkyi@gmail.com>
2023-07-05 21:56:14 -0400

    update repositories (added paginated queries)

--- src/main/java/com/webforum/forum/repository/CategoryRepository.java
+++ src/main/java/com/webforum/forum/repository/CategoryRepository.java
@@ -1,9 +1,15 @@
 package com.webforum.forum.repository;
 
 import com.webforum.forum.entity.Category;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 
 @Repository
 public interface CategoryRepository extends ReactiveCrudRepository<Category, Long> {
+
+    @Query("SELECT * FROM category LIMIT :#{#pageable.pageSize} OFFSET :#{#pageable.offset}")
+    Flux<Category> findAll(Pageable pageable);
 }
--- src/main/java/com/webforum/forum/repository/ThreadRepository.java
+++ src/main/java/com/webforum/forum/repository/ThreadRepository.java
@@ -1,9 +1,15 @@
 package com.webforum.forum.repository;
 
 import com.webforum.forum.entity.Thread;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 
 @Repository
 public interface ThreadRepository extends ReactiveCrudRepository<Thread, Long> {
+
+    @Query("SELECT * FROM thread LIMIT :#{#pageable.pageSize} OFFSET :#{#pageable.offset}")
+    Flux<Thread> findAll(Pageable pageable);
 }
